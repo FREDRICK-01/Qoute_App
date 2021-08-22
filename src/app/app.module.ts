11e0b26d0326adb8a3289e44cbc5eaa04566547b
FREDRICK-01 <fredrickgachuru73@gmail.com>
2021-08-22 07:16:11 +0300

    adds a Quote form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QouteInfoComponent } from './qoute-info/qoute-info.component';
 import { QouteComponent } from './qoute/qoute.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QouteInfoComponent,
-    QouteComponent
+    QouteComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
